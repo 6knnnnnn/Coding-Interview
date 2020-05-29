2261caa7b71c806018a926d633f81a31d7cd802b
liukun1016 <liukun1016@gmail.com>
2020-05-28 21:08:06 -0400

    word square

--- 字符串匹配/WordSquare.py
+++ 字符串匹配/WordSquare.py
@@ -8,10 +8,23 @@ DFS + pruning 剪枝
 
 def valid_word_square(words):
     """
-
+    https://leetcode.com/problems/valid-word-square
     :param words:
     :return:
     """
+    def check(words, i):
+        j = 0
+        for c in words[i]:
+            if j == len(words) or i >= len(words[j]) or c != words[j][i]:
+                return False
+            j += 1
+
+        return True
+
+    for i in xrange(len(words)):
+        if not check(words, i):
+            return False
+    return True
 
 
 def word_square(words):
