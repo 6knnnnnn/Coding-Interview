9082e08d7cf0a8fd92ec4cc44078702c5d6e32b4
Kun Liu <kunliu@uber.com>
2020-06-29 12:47:00 -0400

    Wish 6/29

--- company/Wish/2048游戏.py
+++ company/Wish/2048游戏.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 from random import randint
+from collections import defaultdict
 
 
 class ValueCell(object):
     def __init__(self, x, y, val=0):
         self.x, self.y, self.val = x, y, val
         self.key = ValueCell.getKey(self.x, self.y)
+
+    def __repr__(self):
+        return "{} {} = {}".format(self.x, self.y, self.val)
 
     @staticmethod
     def getKey(x, y):
@@ -15,7 +19,7 @@ class ValueCell(object):
 class GameLogic(object):
     def __init__(self, m, n):
         self.m, self.n = m, n
-        self.values = set([])
+        self.valueCount = defaultdict(int)
         self.availableCells = dict([])
         self.occupiedCells = dict([])
         for i in xrange(m):
@@ -46,7 +50,7 @@ class GameLogic(object):
         return len(self.availableCells) == 0
 
     def has2048(self):
-        return 2048 in self.values
+        return self.valueCount.get(2048) > 0
 
     def canCellMove(self, vc):
         for i, j in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
@@ -68,12 +72,19 @@ class GameLogic(object):
 
     def mergeCell(self, c1, c2):
         # merge c1 into c2, assuming they are both in occupied cells
+        # update value count
+        mergedVal = c1.val + c2.val
+        self.valueCount[c1.val] -= 1
+        self.valueCount[c2.val] -= 1
+        self.valueCount[mergedVal] += 1
+        # merge c2
+        self.occupiedCells[c2.key].val = mergedVal
+        # process c1
         self.occupiedCells.pop(c1.key)
-        self.occupiedCells[c2.key].val += c1.val
         self.availableCells[c1.key] = c1
         c1.val = 0
 
-    def __str__(self):
+    def __repr__(self):
         matrix = [[0] * self.n] * self.m
         for cell in self.occupiedCells.values():
             matrix[cell.x][cell.y] = cell.val
@@ -81,7 +92,11 @@ class GameLogic(object):
             matrix[cell.x][cell.y] = cell.val
         return matrix
 
-moveList = [[1, 1]]
+    def __str__(self):
+        return self.__repr__()
+
+
+moveList = [[1, 1], [0, 1]]
 game = GameLogic(4, 4)
 moveIndex = 0
 while not game.isBoardFull() or not game.has2048() and moveIndex < len(moveList):
